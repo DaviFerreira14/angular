8db37af3aa74a25450b72bfc4757d69723bc3cf0
DaviFerreira14 <davi073ferreira@gmail.com>
2025-07-10 22:49:40 -0300

    Alterações na menu e melhorias nos cards, na tabela e no titulo da pagina Dashboard

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -18,8 +18,8 @@ export class DashboardComponent implements OnInit {
   vinDataList: any[] = [];
 
   vehicleVins: { [key: string]: string[] } = {
-    Ranger: ['2FRHDUYS2Y63NHD22454', '2FRHDUYS2Y63NHD22654'],
-    Mustang: ['2RFAASDY54E4HDU34874', '2RFAASDY54E4HDU34875'],
+    Ranger: ['2FRHDUYS2Y63NHD22454'],
+    Mustang: ['2RFAASDY54E4HDU34874'],
     Territory: ['2FRHDUYS2Y63NHD22455'],
     'Bronco Sport': ['2FRHDUYS2Y63NHD22854'],
   };
